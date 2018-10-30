4d46b09c026448b6160778098d3190149b78a55a
castrolol <lu4n.k4str0@gmail.com>
2018-10-30 19:03:04 -0400

    Docs updated

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,14 +1,14 @@
 export const imports = {
-  'packages/@nimbos/styles/readme.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-styles-readme" */ 'packages/@nimbos/styles/readme.mdx'),
-  'packages/@nimbos/button/docs/button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-button-docs-button" */ 'packages/@nimbos/button/docs/button.mdx'),
-  'packages/@nimbos/entry/docs/entry.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-entry-docs-entry" */ 'packages/@nimbos/entry/docs/entry.mdx'),
-  'packages/@nimbos/entry/docs/label.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-entry-docs-label" */ 'packages/@nimbos/entry/docs/label.mdx'),
-  'packages/@nimbos/i18n/docs/i18n.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-i18n-docs-i18n" */ 'packages/@nimbos/i18n/docs/i18n.mdx'),
-  'packages/@nimbos/layout/docs/layout.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "packages-nimbos-layout-docs-layout" */ 'packages/@nimbos/layout/docs/layout.mdx'),
+  'packages/styles/readme.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-styles-readme" */ 'packages/styles/readme.mdx'),
+  'packages/button/docs/button.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-button-docs-button" */ 'packages/button/docs/button.mdx'),
+  'packages/entry/docs/entry.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-entry-docs-entry" */ 'packages/entry/docs/entry.mdx'),
+  'packages/entry/docs/label.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-entry-docs-label" */ 'packages/entry/docs/label.mdx'),
+  'packages/i18n/docs/i18n.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-i18n-docs-i18n" */ 'packages/i18n/docs/i18n.mdx'),
+  'packages/layout/docs/layout.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "packages-layout-docs-layout" */ 'packages/layout/docs/layout.mdx'),
 }
