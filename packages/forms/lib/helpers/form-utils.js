a76fd3c00d3e34f5fe0ff305845a7e5348b1fcaa
castrolol <lu4n.k4str0@gmail.com>
2018-11-29 18:04:08 -0300

    fix forms

--- packages/forms/lib/helpers/form-utils.js
+++ packages/forms/lib/helpers/form-utils.js
@@ -81,7 +81,7 @@ export function mapFormSimpleProps(props) {
   var rawData = wrapFormData(data);
 
   var rawOnChange = function rawOnChange(data) {
-    return onChange(data);
+    return onChange(unwrapFormData(data));
   };
 
   return _objectSpread({}, p, {
